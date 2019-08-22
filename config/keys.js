c3a8d14909898b3fc1e98acdbaa0cfc3f45480af
Abbyyuan01 <Abbyyuan01@gmail.com>
2019-08-22 17:48:16 +1000

    Adjust the directory

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: "mongodb://localhost:27017/ghostzen",
+  mongoURI:"mongodb+srv://ghostzen:ghostzen111@cluster0-tvhfs.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
