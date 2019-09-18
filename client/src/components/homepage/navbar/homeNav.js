946cab3771ffd9e7c1bfc91f388e52139a755c13
Nishasvl <48741811+Nishasvl@users.noreply.github.com>
2019-09-18 17:04:05 +1000

    Update homeNav.js

--- client/src/components/homepage/navbar/homeNav.js
+++ client/src/components/homepage/navbar/homeNav.js
@@ -7,39 +7,43 @@ class HomeNav extends Component {
   render() {
     return (
       <div className="demo-big-content">
-          <Layout>
-            <Header
-              className="header-color"
-              title={
-                <Link style={{ textDecoration: "none", color: "white" }} to="/">
-                  Family Tree
-                </Link>
-              }
-              scroll
-            >
-              <Navigation>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/contactus">Contact Us</Link>
-              </Navigation>
-            </Header>
-            {/* <Drawer
-              title={
-                <Link style={{ textDecoration: "none", color: "black" }} to="/">
-                  Family Tree
-                </Link>
-              }
-            >
-              <Navigation>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/contactus">Contact Us</Link>
-              </Navigation>
-            </Drawer> */}
-          </Layout>
+        <Layout>
+          <Header
+            className="header-color"
+            title={
+              <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                Family Tree
+              </Link>
+            }
+            scroll
+          >
+            <Navigation>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/contactus">Contact Us</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </Navigation>
+          </Header>
+          <Drawer
+            title={
+              <Link style={{ textDecoration: "none", color: "black" }} to="/">
+                Family Tree
+              </Link>
+            }
+          >
+            <Navigation>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/contactus">Contact Us</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </Navigation>
+          </Drawer>{" "}
+          */
+        </Layout>
       </div>
-      
     );
   }
 }
